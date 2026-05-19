6806cb382f215009ab4c43c9ee857edab1470209
Bildan Jauhary <bildanjauhary@gmail.com>
2026-05-19 13:17:48 +0700

    Add files via upload

---
+++ utils/moment.js
@@ -0,0 +1,10 @@
+import moment from "moment";
+
+export default function generateDate(date) {
+    const dates = moment(date, "DD-MM-YYYY", true)
+    if(!dates.isValid()){
+        return console.log('Format tanggal tidak sesuai')
+    }
+
+    return console.log('Hasil :'+ dates.calendar())
+}

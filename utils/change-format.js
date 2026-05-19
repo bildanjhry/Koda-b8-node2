b61427d0d91505692d71f7b65484b244e18cbfc1
Bildan Jauhary <bildanjauhary@gmail.com>
2026-05-19 15:04:18 +0700

    Refactor changeFormat function for clarity

--- utils/change-format.js
+++ utils/change-format.js
@@ -5,12 +5,6 @@ export default function changeFormat(date) {
   if(isNaN(newDates.valueOf())){
     return console.log('Format tidak sesuai');
   }
-
-  // function isValidDate(newDates) {
-  //     return isNaN(Date.parse(newDates));
-    
-  // }
-
     
   const formatter = new Intl.DateTimeFormat('en-US', format, { dateStyle: 'long' });
   const formatedDates = formatter.format(newDates);
